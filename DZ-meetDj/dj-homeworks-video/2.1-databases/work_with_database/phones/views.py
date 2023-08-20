6776abacdc4a25d45a43b4dce7002e9e614e831b
Astapot <myagkovmacsim@yandex.ru>
2023-08-20 13:26:25 +0300

    fix

--- DZ-meetDj/dj-homeworks-video/2.1-databases/work_with_database/phones/views.py
+++ DZ-meetDj/dj-homeworks-video/2.1-databases/work_with_database/phones/views.py
@@ -9,7 +9,14 @@ def show_catalog(request):
     sort = request.GET.get('sort')
     template = 'catalog.html'
     phones = Phone.objects.all()
-    print(phones)
+    if sort == 'min_price':
+        phones = Phone.objects.order_by('price')
+    elif sort == 'name':
+        phones = Phone.objects.order_by('name')
+    elif sort == 'max_price':
+        phones = Phone.objects.order_by('-price')
+    else:
+        phones = Phone.objects.all()
     context = {'phones': phones}
     return render(request, template, context)
 
