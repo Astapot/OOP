a21768daa4e9fd9de4eee5e0b8f8d58682b8a45a
Astapot <myagkovmacsim@yandex.ru>
2023-08-29 15:09:04 +0300

    fixedddd

--- DZ-Django/dj-homeworks-video/2.2-databases-2/m2m-relations/articles/admin.py
+++ DZ-Django/dj-homeworks-video/2.2-databases-2/m2m-relations/articles/admin.py
@@ -2,26 +2,31 @@ from django.contrib import admin
 from django.core.exceptions import ValidationError
 from django.forms import BaseInlineFormSet
 from .models import Article, Tags, TagsArticle
+from django.forms import inlineformset_factory
 
 
 class TagsArticleInlineFormset(BaseInlineFormSet):
     def clean(self):
+        is_main_counter = 0
         for form in self.forms:
             # В form.cleaned_data будет словарь с данными
             # каждой отдельной формы, которые вы можете проверить
-            print(form)
             form.cleaned_data
             print(form.cleaned_data)
+            if form.cleaned_data.get('is_main'):
+                is_main_counter += 1
             # вызовом исключения ValidationError можно указать админке о наличие ошибки
             # таким образом объект не будет сохранен,
             # а пользователю выведется соответствующее сообщение об ошибке
+            print(is_main_counter)
+        if is_main_counter > 1:
             raise ValidationError('Основной раздел может быть только один')
         return super().clean()  # вызываем базовый код переопределяемого метода
 
 
 class TagsArticleInline(admin.TabularInline):
     model = TagsArticle
-
+    formset = TagsArticleInlineFormset
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
--- DZ-Django/dj-homeworks-video/2.2-databases-2/m2m-relations/articles/views.py
+++ DZ-Django/dj-homeworks-video/2.2-databases-2/m2m-relations/articles/views.py
@@ -5,11 +5,12 @@ from articles.models import Article
 
 def articles_list(request):
     template = 'articles/news.html'
-    object_list = Article.objects.all()
+    ordering = '-published_at'
+    object_list = Article.objects.all().order_by(ordering)
     context = {'object_list': object_list}
 
     # используйте этот параметр для упорядочивания результатов
     # https://docs.djangoproject.com/en/3.1/ref/models/querysets/#django.db.models.query.QuerySet.order_by
-    # ordering = '-published_at'
+
 
     return render(request, template, context)
