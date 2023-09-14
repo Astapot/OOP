e8bb9eaf799631457ba83d5f88a1d140611ac36b
Astapot <myagkovmacsim@yandex.ru>
2023-09-14 23:10:32 +0300

    fix

--- DZ-Django/dj-homeworks-video/3.3-permissions/api_with_restrictions/advertisements/serializers.py
+++ DZ-Django/dj-homeworks-video/3.3-permissions/api_with_restrictions/advertisements/serializers.py
@@ -36,32 +36,43 @@ class AdvertisementSerializer(serializers.ModelSerializer):
         # обратите внимание на `context` – он выставляется автоматически
         # через методы ViewSet.
         # само поле при этом объявляется как `read_only=True`
-        advs = Advertisement.objects.all().filter(creator=self.context["request"].user)
+        # advs = Advertisement.objects.all().filter(creator=self.context["request"].user)
         # print(advs)
-        OPENED = 0
-        for adv in advs:
-            if adv.status == 'OPEN':
-                OPENED += 1
-        validated_data["creator"] = self.context["request"].user
-        if OPENED >= 10:
-            raise ValidationError('Количество открытых объявлений не может превышать 10 штук')
+        # OPENED = 0
+        # for adv in advs:
+        #     if adv.status == 'OPEN':
+        #         OPENED += 1
+        # validated_data["creator"] = self.context["request"].user
+        # if OPENED >= 10:
+
+        # код выше оставил для себя, на будущее
+
+        # opened = Advertisement.objects.all().filter(creator=self.context["request"].user, status='OPEN').count()
+        # if opened >= 10:
+        #     raise ValidationError('Количество открытых объявлений не может превышать 10 штук')
         return super().create(validated_data)
 
     def validate(self, data):
         """Метод для валидации. Вызывается при создании и обновлении."""
-
+        opened = Advertisement.objects.all().filter(creator=self.context["request"].user, status='OPEN').count()
+        if opened >= 10 and data.get('status') != 'CLOSED':
+            raise ValidationError('Количество открытых объявлений не может превышать 10 штук')
         # TODO: добавьте требуемую валидацию
         return data
 
     def update(self, instance, validated_data):
-        advs = Advertisement.objects.all().filter(creator=self.context["request"].user)
-        OPENED = 0
-        for adv in advs:
-            if adv.status == 'OPEN':
-                OPENED += 1
-        # print(OPENED)
-        if OPENED >= 10 and validated_data.get('status') and validated_data.get('status') != 'CLOSED':
-            raise ValidationError('Количество открытых объявлений не может превышать 10 штук')
+        # advs = Advertisement.objects.all().filter(creator=self.context["request"].user)
+        # OPENED = 0
+        # for adv in advs:
+        #     if adv.status == 'OPEN':
+        #         OPENED += 1
+        # # print(OPENED)
+        # if OPENED >= 10 and validated_data.get('status') and validated_data.get('status') != 'CLOSED':
+
+
+        # opened = Advertisement.objects.all().filter(creator=self.context["request"].user, status='OPEN').count()
+        # if opened >= 10 and validated_data.get('status') != 'CLOSED':
+        #     raise ValidationError('Количество открытых объявлений не может превышать 10 штук')
         instance.title = validated_data.get('title', instance.title)
         instance.description = validated_data.get('description', instance.description)
         instance.status = validated_data.get('status', instance.status)
--- DZ-Django/dj-homeworks-video/3.3-permissions/api_with_restrictions/advertisements/views.py
+++ DZ-Django/dj-homeworks-video/3.3-permissions/api_with_restrictions/advertisements/views.py
@@ -25,7 +25,7 @@ class AdvertisementViewSet(ModelViewSet):
 
     def get_permissions(self):
         """Получение прав для действий."""
-        if self.action in ["create", "update", "partial_update"]:
+        if self.action in ["create", "update", "partial_update", 'destroy']:
             return [IsAuthenticated(), IsOwner()]
         return []
 
